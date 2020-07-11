aaedfc505400f421b293b8aa8c2468e2060cf785
trinko <trinko@tiscali.it>
2020-07-11 19:27:11 +0200

    Aggiornamento copyright

--- src/Repository/UtenteRepository.php
+++ src/Repository/UtenteRepository.php
@@ -2,11 +2,11 @@
 /**
  * giua@school
  *
- * Copyright (c) 2017-2019 Antonello Dessì
+ * Copyright (c) 2017-2020 Antonello Dessì
  *
  * @author    Antonello Dessì
  * @license   http://www.gnu.org/licenses/agpl.html AGPL
- * @copyright Antonello Dessì 2017-2019
+ * @copyright Antonello Dessì 2017-2020
  */
 
 
@@ -14,12 +14,30 @@ namespace App\Repository;
 
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 
 
 /**
  * Utente - repository
  */
-class UtenteRepository extends EntityRepository {
+class UtenteRepository extends EntityRepository implements UserLoaderInterface {
+
+  /**
+   * Carica utente da username o email (usato dai moduli di sicurezza)
+   *
+   * @param string $username Parametro per identificare l'utente (username o email)
+   *
+   * @return Utente|null Restitusce l'utente trovato o null
+   */
+  public function loadUserByUsername($username) {
+    $utente = $this->createQueryBuilder('u')
+      ->where('u.username=:utente OR u.email=:utente')
+      ->setParameter('utente', $username)
+      ->getQuery()
+      ->getOneOrNullResult();
+    // restituisce dati
+    return $utente;
+  }
 
   /**
    * Paginatore dei risultati della query
@@ -54,4 +72,3 @@ class UtenteRepository extends EntityRepository {
   }
 
 }
-
