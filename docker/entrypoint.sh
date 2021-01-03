6e5cf5d6cbf52466a1498583c5a218b5f4f5f80f
trinko <trinko@tiscali.it>
2021-01-03 23:56:57 +0100

    update docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+### Start services
+#service apache2 start
+service mysql start
+
+### Configure Apache
+
+### Configure PHP
+
+### Configure Mysql
+mysqladmin -u root password root
+php bin/console doctrine:database:create -e test
+php bin/console doctrine:schema:update -f -e test
