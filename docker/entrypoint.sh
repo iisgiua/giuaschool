5a611aa0f4708519b673b82e75750010f2b2711d
trinko <trinko@tiscali.it>
2021-01-09 21:19:28 +0100

    update docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-### Start services
-#service apache2 start
-service mysql start
-
 ### Configure Apache
+a2enmod rewrite
+sed -r -i -e "s/^;date\.timezone\s*=.*$/date.timezone = Europe\/Rome/" /etc/php/7.4/apache2/php.ini
+sed -r -i -e "s/;opcache.enable=1.*$/opcache.enable=1/" /etc/php/7.4/apache2/php.ini
+sed -r -i -e "s/^;?date\.timezone\s*=.*$/date.timezone = Europe\/Rome/" /etc/php/7.4/cli/php.ini
+
+
+
+### Start services
+service apache2 start
+#service mysql start
+
 
 ### Configure PHP
 
 ### Configure Mysql
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
+#mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 
-php bin/console doctrine:database:create -e dev
-php bin/console doctrine:schema:update -f -e dev
+#php bin/console doctrine:database:create -e dev
+#php bin/console doctrine:schema:update -f -e dev
 
 exit 0
