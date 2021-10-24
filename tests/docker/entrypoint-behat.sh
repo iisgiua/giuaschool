5c02f177f4815ec41a1e4da9ba625d872e3da32a
trinko <trinko@tiscali.it>
2021-10-24 13:20:14 +0200

    Fix docker tests

--- tests/docker/entrypoint-behat.sh
+++ tests/docker/entrypoint-behat.sh
@@ -5,5 +5,4 @@ service mysql start
 service apache2 start
 
 # Run Behat tests
-#-- exec php -d memory_limit=-1 vendor/bin/behat --stop-on-failure -f progress || exit 1
-exec php vendor/bin/behat -f progress || exit 1
+exec php -d memory_limit=-1 vendor/bin/behat --stop-on-failure -f progress
