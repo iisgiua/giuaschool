1cb619dc5011f881bfa06bc7117690727bd5e0b8
trinko <trinko@tiscali.it>
2021-10-25 20:24:17 +0200

    Docker monitor resources

--- tests/docker/entrypoint-behat.sh
+++ tests/docker/entrypoint-behat.sh
@@ -5,5 +5,5 @@ service mysql start
 service apache2 start
 
 # Run Behat tests
-while [ true ] ; do echo ; df -h ; free -h ; ps aux ; echo "---------" ; done &
+while [ true ] ; do echo ; df -h ; free -h ; ps aux ; echo "---------" ; sleep 30 ; done &
 exec php -d memory_limit=-1 vendor/bin/behat --stop-on-failure -f progress
