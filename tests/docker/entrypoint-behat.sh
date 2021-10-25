b9ffd05d9ec82c2d74c90365fbcc69b6f5fe4b8a
trinko <trinko@tiscali.it>
2021-10-25 09:00:58 +0200

    Docker monitor resources

--- tests/docker/entrypoint-behat.sh
+++ tests/docker/entrypoint-behat.sh
@@ -5,7 +5,5 @@ service mysql start
 service apache2 start
 
 # Run Behat tests
-free -h
-swapon --show
-free -h -s 30 &
+while [ true ] ; do echo ; df -h ; free -h ; ps aux ; echo "---------" ; loop &
 exec php -d memory_limit=-1 vendor/bin/behat --stop-on-failure -f progress
--- tests/docker/entrypoint-test.sh
+++ tests/docker/entrypoint-test.sh
@@ -5,7 +5,4 @@ service mysql start
 service apache2 start
 
 # Run Unit tests
-free -h
-swapon --show
-free -h -s 30 &
 exec php -d memory_limit=-1 bin/phpunit --coverage-clover clover.xml
