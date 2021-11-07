84ece1111ce7ee14e04e23c7db68da7931eb4141
trinko <trinko@tiscali.it>
2021-11-07 21:16:09 +0100

    Gestione release su GitHub

---
+++ tests/docker/entrypoint-release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Clear installation files
+rm -f -r tests/
+rm -f -r var/cache/*
+rm -f -r var/sessions/*
+rm -f -r var/log/*
+rm -f bin/phpunit
+rm -f .dockerignore .env.test .gitignore behat.yml composer.* phpunit.xml symfony.lock
+rm -f -r ./*/*/.gitkeep ./*/*/*/.gitkeep
+
+# Archive release
+zip -v -y -r giuaschool-release.zip ./
+tar -zcf giuaschool-release.tgz ./
