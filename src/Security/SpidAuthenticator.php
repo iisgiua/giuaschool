69c72d854e609b2cc1aee62efa6f654057a5760c
trinko <trinko@tiscali.it>
2022-01-10 21:14:30 +0100

    Aggiornamento copyright

--- src/Security/SpidAuthenticator.php
+++ src/Security/SpidAuthenticator.php
@@ -2,11 +2,11 @@
 /**
  * giua@school
  *
- * Copyright (c) 2017-2021 Antonello Dessì
+ * Copyright (c) 2017-2022 Antonello Dessì
  *
  * @author    Antonello Dessì
  * @license   http://www.gnu.org/licenses/agpl.html AGPL
- * @copyright Antonello Dessì 2017-2021
+ * @copyright Antonello Dessì 2017-2022
  */
 
 
