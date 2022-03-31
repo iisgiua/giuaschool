ed20bafad5f8a8065d1f7c8ff9a21cf6ba679cd7
trinko <trinko@tiscali.it>
2022-03-31 10:50:38 +0200

    Fix: visualizzazione doppia di alunni in agenda quando selezionati compiti/verifiche

--- src/Util/BachecaUtil.php
+++ src/Util/BachecaUtil.php
@@ -93,7 +93,7 @@ class BachecaUtil {
     } elseif ($avviso->getFiltroTipo() == 'U') {
       // filtro utenti
       $dati['utenti'] = $this->em->getRepository('App:Alunno')->createQueryBuilder('a')
-        ->select('a.cognome,a.nome,a.dataNascita,c.anno,c.sezione,aa.letto,ag1.letto AS letto_genitore1,ag2.letto AS letto_genitore2')
+        ->select('DISTINCT a.cognome,a.nome,a.dataNascita,c.anno,c.sezione,aa.letto,ag1.letto AS letto_genitore1,ag2.letto AS letto_genitore2')
         ->join('a.classe', 'c')
         ->join('App:Genitore', 'g1', 'WITH', 'g1.alunno=a.id')
         ->leftJoin('App:Genitore', 'g2', 'WITH', 'g2.alunno=a.id AND g2.id!=g1.id')
