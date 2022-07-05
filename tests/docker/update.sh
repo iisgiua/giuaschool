4fe8d11e63965c8e38898755f919bb857da10b03
trinko <trinko@tiscali.it>
2022-07-05 18:54:52 +0200

    update GHA

--- tests/docker/update.sh
+++ tests/docker/update.sh
@@ -13,5 +13,9 @@ rm -f -r ./*/*/.gitkeep ./*/*/*/.gitkeep
 # Rename .env to avoid overwriting on update
 mv .env .env-dist
 
-# Create archive for all files
-zip -q -r -y -9 giuaschool-update.zip .
+# Create archive for added/changed files
+wget -q -P ../ https://github.com/trinko/giuaschool/releases/download/$1/giuaschool-release-$1.md5.zip
+unzip -q ../giuaschool-release-$1.md5.zip -d ../
+find ./ -type f -exec md5sum {} + | sort -k 2 > ../giuaschool-update.md5
+diff -n ../giuaschool-release.md5 ../giuaschool-update.md5 | sed -E '/^.{,32}$/d' | sed -E 's/^.*\s+//' > ../lista.txt
+cat ../lista.txt | zip -q -y -9 -@ ../giuaschool-update.zip
