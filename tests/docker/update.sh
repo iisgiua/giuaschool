b24a92eed273d138834d876b7906d11baa320838
trinko <trinko@tiscali.it>
2022-08-07 18:43:55 +0200

    Aggiornamento repository

--- tests/docker/update.sh
+++ tests/docker/update.sh
@@ -9,7 +9,7 @@ rm -f -r var/cache/* var/log/* var/sessions/*
 mv .env .env-dist
 
 # Create archive for added/changed files
-wget -q -P ../ https://github.com/trinko/giuaschool/releases/download/$1/giuaschool-release-$1.md5.zip
+wget -q -P ../ https://github.com/iisgiua/giuaschool/releases/download/$1/giuaschool-release-$1.md5.zip
 unzip -q ../giuaschool-release-$1.md5.zip -d ../
 find ./ -type f -exec md5sum {} + | sort -k 2 > ../giuaschool-update.md5
 diff -n ../giuaschool-release.md5 ../giuaschool-update.md5 | sed -E '/^.{,32}$/d' | sed -E 's/^.*\s+//' > ../lista.txt
