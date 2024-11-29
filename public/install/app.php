5015f65833f8fdfdcccc700c0286e967b84607d4
trinko <trinko@tiscali.it>
2024-11-30 00:29:08 +0100

    Fix: controllo versione PHP

--- public/install/app.php
+++ public/install/app.php
@@ -1,5 +1,8 @@
 <?php
 
+if (version_compare(PHP_VERSION, '8.2', '<')) {
+  die("*** ERRORE CRITICO<br>L'applicazione richiede PHP 8.2")
+}
 require dirname(__DIR__, 2).'/src/Install/Updater.php';
 use App\Install\Updater;
 
