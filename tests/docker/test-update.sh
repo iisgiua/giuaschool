c9fe0f425c8ea471176eaa44fa99369e8e69f513
trinko <trinko@tiscali.it>
2025-09-05 18:12:55 +0200

    Update docker

--- tests/docker/test-update.sh
+++ tests/docker/test-update.sh
@@ -6,10 +6,6 @@ service apache2 start
 dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
 google-chrome --headless --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --window-size=1920,1080 --ignore-certificate-errors 2> /dev/null &
 
-# Make sure test/prod directories exist
-mkdir -p var/sessions/test
-chown -R www-data:www-data var/*
-
 # test step 1: unzip files
 su -s /bin/bash -p -c "php -d memory_limit=-1 vendor/bin/behat tests/features/test-update-1.feature --stop-on-failure -f progress" www-data
 retval=$?
