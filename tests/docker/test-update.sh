89a87b625ca842e9218a6b0264cf0ad356dfa256
trinko <trinko@tiscali.it>
2025-09-06 20:48:50 +0200

    Fix tests

--- tests/docker/test-update.sh
+++ tests/docker/test-update.sh
@@ -8,7 +8,7 @@ service mariadb start
 service apache2 start
 dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
 google-chrome --headless --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --window-size=1920,1080 --ignore-certificate-errors 2> /dev/null &
-rm -r var/cache/test/* tests/temp/*
+rm -rf var/cache/test/* tests/temp/*
 
 # passo 1: estrae file zip
 su -s /bin/bash -p -c "php -d memory_limit=-1 vendor/bin/behat tests/features/test-update-1.feature --stop-on-failure -f progress" www-data
@@ -17,7 +17,7 @@ if [ $retval -ne 0 ]; then
   exit 1
 fi
 composer -q install --no-progress --no-scripts
-rm -r var/cache/test/* tests/temp/*
+rm -rf var/cache/test/* tests/temp/*
 
 # passi successivi
 su -s /bin/bash -p -c "php -d memory_limit=-1 vendor/bin/behat tests/features/test-update-2.feature --stop-on-failure -f progress" www-data
@@ -25,7 +25,7 @@ retval=$?
 if [ $retval -ne 0 ]; then
   exit 1
 fi
-rm -r var/cache/test/* tests/temp/*
+rm -rf var/cache/test/* tests/temp/*
 
 # smoke test
 su -s /bin/bash -p -c "php -d memory_limit=-1 vendor/bin/behat tests/features/test-update-3.feature --stop-on-failure -f progress" www-data
