dfb1e67e9b2dc46732f57155f608116cf9b4f964
trinko <trinko@tiscali.it>
2025-09-06 15:28:26 +0200

    Fix tests

--- tests/docker/test-phpunit.sh
+++ tests/docker/test-phpunit.sh
@@ -7,7 +7,7 @@ set -e
 service mariadb start
 service apache2 start
 dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
-php bin/consonle cache:warmup
+php bin/console cache:warmup
 
 # Resegue test PHPUnit
 php -d memory_limit=-1 vendor/bin/phpunit --coverage-clover clover.xml
