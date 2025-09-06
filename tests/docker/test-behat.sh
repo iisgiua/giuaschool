45cb246e2444d06f68678cb35d7eb00a18986600
trinko <trinko@tiscali.it>
2025-09-06 20:28:17 +0200

    Fix docker

--- tests/docker/test-behat.sh
+++ tests/docker/test-behat.sh
@@ -8,6 +8,7 @@ service mariadb start
 service apache2 start
 dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
 google-chrome --headless --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --window-size=1920,1080 --ignore-certificate-errors 2> /dev/null &
+php bin/console cache:clear -q
 
 # esegue test Behat
 php -d memory_limit=-1 vendor/bin/behat $1 --stop-on-failure -f progress
