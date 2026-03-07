45b60f9a9c263656e87597a6916dd12bd73175af
trinko <trinko@tiscali.it>
2026-03-07 08:37:10 +0100

    Rimosso: vecchio supporto SPID

--- src/Controller/SpidController.php
+++ src/Controller/SpidController.php
@@ -24,58 +24,58 @@ class SpidController extends BaseController {
 
   //==================== ATTRIBUTI DELLA CLASSE  ====================
 
-  /**
-   * @var array $idp Lista IDP per lo SPID
-   */
-  private $idp;
+  // /**
+  //  * @var array $idp Lista IDP per lo SPID
+  //  */
+  // private $idp;
 
 
   //==================== METODI DELLA CLASSE ====================
 
-  /**
-   * Costruttore
-   *
-   */
-  public function __construct() {
-    // inizializza
-    $this->idp = [
-      'IdAruba' => 'ArubaPEC S.p.A.',
-      'IdEtna' => 'EtnaHitech S.C.p.A.',
-      'IdInfocert' => 'InfoCert S.p.A.',
-      'IdLepida' => 'Lepida S.p.A.',
-      'IdNamirial' => 'Namirial',
-      'IdPoste' => 'Poste Italiane SpA',
-      'IdSielte' => 'Sielte S.p.A.',
-      'IdRegister' => 'Register.it S.p.A.',
-      'IdTeamsystem' => 'TeamSystem s.p.a.',
-      'IdTim' => 'TI Trust Technologies srl',
-      'IdInfocamere' => 'InfoCamere S.C.p.A.',
-      'IdIntesiGroup' => 'Intesi Group S.p.A.'];
-    }
+  // /**
+  //  * Costruttore
+  //  *
+  //  */
+  // public function __construct() {
+  //   // inizializza
+  //   $this->idp = [
+  //     'IdAruba' => 'ArubaPEC S.p.A.',
+  //     'IdEtna' => 'EtnaHitech S.C.p.A.',
+  //     'IdInfocert' => 'InfoCert S.p.A.',
+  //     'IdLepida' => 'Lepida S.p.A.',
+  //     'IdNamirial' => 'Namirial',
+  //     'IdPoste' => 'Poste Italiane SpA',
+  //     'IdSielte' => 'Sielte S.p.A.',
+  //     'IdRegister' => 'Register.it S.p.A.',
+  //     'IdTeamsystem' => 'TeamSystem s.p.a.',
+  //     'IdTim' => 'TI Trust Technologies srl',
+  //     'IdInfocamere' => 'InfoCamere S.C.p.A.',
+  //     'IdIntesiGroup' => 'Intesi Group S.p.A.'];
+  //   }
 
-  /**
-   * Invia il file XML con i metadata per il service provider SPID
-   * NB: la url "/metadata" è un requisito SPID
-   *
-   * @param Request $request Pagina richiesta
-   *
-   * @return Response Pagina di risposta
-   */
-  #[Route(path: '/metadata', name: 'spid_metadata', methods: ['GET'])]
-  public function metadata(Request $request): Response {
-    if ($request->query->get('debug') == 'yes') {
-      // rigenera metadata
-      return $this->redirect('/spid/module.php/saml/sp/metadata.php/service');
-    }
-    // legge file metadata
-    $xml = file_get_contents($this->getParameter('kernel.project_dir').'/config/metadata/registro-spid.xml');
-    $response = new Response($xml);
-    // invia metadata
-    $response->headers->set('Content-Type', 'application/samlmetadata+xml');
-    $disposition = HeaderUtils::makeDisposition(HeaderUtils::DISPOSITION_ATTACHMENT, 'metadata.xml');
-    $response->headers->set('Content-Disposition', $disposition);
-    return $response;
-  }
+  // /**
+  //  * Invia il file XML con i metadata per il service provider SPID
+  //  * NB: la url "/metadata" è un requisito SPID
+  //  *
+  //  * @param Request $request Pagina richiesta
+  //  *
+  //  * @return Response Pagina di risposta
+  //  */
+  // #[Route(path: '/metadata', name: 'spid_metadata', methods: ['GET'])]
+  // public function metadata(Request $request): Response {
+  //   if ($request->query->get('debug') == 'yes') {
+  //     // rigenera metadata
+  //     return $this->redirect('/spid/module.php/saml/sp/metadata.php/service');
+  //   }
+  //   // legge file metadata
+  //   $xml = file_get_contents($this->getParameter('kernel.project_dir').'/config/metadata/registro-spid.xml');
+  //   $response = new Response($xml);
+  //   // invia metadata
+  //   $response->headers->set('Content-Type', 'application/samlmetadata+xml');
+  //   $disposition = HeaderUtils::makeDisposition(HeaderUtils::DISPOSITION_ATTACHMENT, 'metadata.xml');
+  //   $response->headers->set('Content-Disposition', $disposition);
+  //   return $response;
+  // }
 
   /**
    * Inizia la procedura di login tramite SPID
@@ -84,16 +84,17 @@ class SpidController extends BaseController {
    */
   #[Route(path: '/spid/login/{idp}', name: 'spid_login', methods: ['GET'])]
   public function login(string $idp): Response {
-    $code = $this->idp[$idp] ?? $idp;
-    return $this->redirect('/spid-login.php?idp='.urlencode((string) $code));
+    // $code = $this->idp[$idp] ?? $idp;
+    // return $this->redirect('/spid-login.php?idp='.urlencode((string) $code));
+    return $this->redirectToRoute('mimspid_login');
   }
 
-  /**
-   * Esegue il login sull'applicazione al termine dell'autenticazione SPID
-   */
-  #[Route(path: '/spid/acs/{responseId}', name: 'spid_acs', methods: ['GET'])]
-  public function acs() {
-    // la procedura viene eseguita nella classe SpidAuthenticator
-  }
+  // /**
+  //  * Esegue il login sull'applicazione al termine dell'autenticazione SPID
+  //  */
+  // #[Route(path: '/spid/acs/{responseId}', name: 'spid_acs', methods: ['GET'])]
+  // public function acs() {
+  //   // la procedura viene eseguita nella classe SpidAuthenticator
+  // }
 
 }
