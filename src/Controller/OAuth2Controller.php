7e83cfaba4a3a4bbcf4b6a3b388bbfc3d2069eaf
trinko <trinko@tiscali.it>
2026-08-07 00:17:31 +0200

    Fix

--- src/Controller/OAuth2Controller.php
+++ src/Controller/OAuth2Controller.php
@@ -9,7 +9,6 @@
 namespace App\Controller;
 
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
